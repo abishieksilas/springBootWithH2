6941a518d2b9079b9f4e71efa7376b24c183a585
Abishiek <abishieksilas@gmail.com>
2025-10-06 23:00:21 +0530

    JDBC Template

--- SpringJDBCEx/src/main/java/com/telusko/SpringJDBCEx/repo/StudentRepo.java
+++ SpringJDBCEx/src/main/java/com/telusko/SpringJDBCEx/repo/StudentRepo.java
@@ -1,11 +1,28 @@
 package com.telusko.SpringJDBCEx.repo;
 
 import com.telusko.SpringJDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentRepo {
+
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
     public void save(Student s) {
-        System.out.println("added");
+
+        String query = "insert into student (rollno, name, marks) values (?, ?, ?)";
+        int rows = jdbc.update(query, s.getRollNo(), s.getName(), s.getMarks());
+        System.out.println(rows + " affected");
     }
 }
---
+++ SpringJDBCEx/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into student (rollNo, name, marks) values (101, 'Irene', 75);
+insert into student (rollNo, name, marks) values (102, 'Abi', 85);
+insert into student (rollNo, name, marks) values (103, 'Nandhini', 95);
